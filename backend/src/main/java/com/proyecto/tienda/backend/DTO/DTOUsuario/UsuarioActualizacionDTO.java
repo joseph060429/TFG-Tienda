42089d4f82a71ba3319f67de257080187c071882
joseph060429 <122611124+joseph060429@users.noreply.github.com>
2024-04-16 10:00:03 +0200

    Todo bien, solo falta la tabla devoluciones en el back

--- backend/src/main/java/com/proyecto/tienda/backend/DTO/DTOUsuario/UsuarioActualizacionDTO.java
+++ backend/src/main/java/com/proyecto/tienda/backend/DTO/DTOUsuario/UsuarioActualizacionDTO.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import org.springframework.data.mongodb.core.index.Indexed;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
